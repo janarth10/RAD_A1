1bb358e0ea1e75dbc54908bb3edec130a2b32fea
janarth10 <janarthp10@gmail.com>
2018-03-21 17:58:04 +1100

    Do not need to implement help menu

--- s3706154_p2.rb
+++ s3706154_p2.rb
@@ -2,12 +2,6 @@
 
 require 'nokogiri'
 
-std_num="s3706154_p2.rb"
-
-puts "Commands:"
-puts "#{std_num} -xml [filename]   # Load a XML file"
-puts "#{std_num} help [COMMAND]    # Describe available commands or one specific command"
-
 command = gets.chomp.split(" ")
 if command.size == 3 and command[0] == std_num and command[1] == "-xml"
 	puts "xml option"
