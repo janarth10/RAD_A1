1a4b0e39e0f9128a434e96f95a69a23767e3c1e9
janarth10 <janarthp10@gmail.com>
2018-03-22 11:04:12 +1100

    finished part 2, not ruby-linted though

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # RAD_A1
-Learning how to script in ruby
+Janarth Punniyamoorthy s3706154
+
+- To run the script use ruby s3706154_p2.rb -h to see how to pass arguments.
+
+- For the flags always use two hyphens(EG: good: --xml emails.xml  bad: -xml emails.xml)
+
+- Also when entering names with an apostrophe, prefix the apostrophe with a backslash(EG O\'Donnelly)
+
+- flags can be entered in any order but "list" must be entered immediately after "ruby s3706154_p2.rb"
--- s3706154_p2.rb
+++ s3706154_p2.rb
@@ -4,11 +4,93 @@ require 'nokogiri'
 require 'optparse'
 
 options = {}
-OptionParser.new do |opt|
-	opt.on("--xml [FILENAME]", "loads given filename, or uses first xml file in directory if flag not used") { |o|
-		options[:xml_file] = o}
-	opt.on("--name FIRST/LAST NAME", "Searches for emails with the given name") { |o| options[:name] = o}
-  opt.on("--ip IP ADDRESS", "Searches for email with the given ip address") { |o| options[:ip_addr] = o}
-end.parse!
+begin
+  OptionParser.new do |opt|
+    opt.on("--xml [FILENAME]", "loads given filename, or uses first xml file in directory if flag not used") { |o|
+      options[:xml_file] = o}
+    opt.on("--name FIRST/LAST NAME", "Searches for emails with the given name") { |o| options[:name] = o}
+    opt.on("--ip IP ADDRESS", "Searches for email with the given ip address") { |o| options[:ip_addr] = o}
+  end.parse!
 
+  if options.has_key?(:xml_file)
+    file_name = options[:xml_file]
+  elsif
+    # check for first xml file if not provided
+    Dir.entries(".").each do |entry|
+      if entry.to_s.chars.last(4).join == ".xml"
+        file_name = entry.to_s
+      end
+    end
+  end
+
+  doc = Nokogiri::XML(open(file_name))
+  nodes =  doc.xpath("//record")
+  nodes = nodes[0..3]
+  first_names = doc.xpath("//first_name")
+  last_names = doc.xpath("//last_name")
+  ip_addr_lst = doc.xpath("//ip_address")
+
+  if options.has_key?(:name)
+    i = 0
+    nodes.size.times do
+      if first_names[i].children.to_s.casecmp(options[:name]) != 0 and
+          last_names[i].children.to_s.casecmp(options[:name]) != 0
+        nodes.delete(nodes[i])
+        first_names.delete(first_names[i])
+        last_names.delete(last_names[i])
+        ip_addr_lst.delete(ip_addr_lst[i])
+      elsif
+        i += 1
+      end
+    end
+  end
+
+  if options.has_key?(:ip_addr)
+    i = 0
+    nodes.size.times do
+      if ip_addr_lst[i].children.to_s.casecmp(options[:ip_addr]) != 0
+        nodes.delete(nodes[i])
+        first_names.delete(first_names[i])
+        last_names.delete(last_names[i])
+        ip_addr_lst.delete(ip_addr_lst[i])
+      elsif
+      i += 1
+      end
+    end
+  end
+
+  # input: nodeset of Nokogiri::XML::Element
+  def puts_xml_to_json(arr)
+    arr.each do |elem|
+      str = elem.to_s
+      tag, val = extract_tag_and_value(str)
+      if elem == arr.first
+        puts "{ #{tag}: #{val},"
+      elsif elem == arr.last
+        puts "#{tag}: #{val}\n},"
+      else
+        puts "#{tag}: #{val},"
+      end
+    end
+  end
+
+  def extract_tag_and_value(str)
+    end_of_first_tag = str.index(">")
+    beg_of_second_tag = str.index("</")
+
+    tag = "\"#{str[1..end_of_first_tag-1]}\""
+    value = "#{str[end_of_first_tag+1..beg_of_second_tag-1]}"
+    if tag != "\"id\""
+      value = "\"#{value}\""
+    end
+    return tag, value
+  end
+
+  nodes.each do |elem|
+    puts_xml_to_json(elem.children)
+  end
+
+rescue StandardError => e
+  puts e.message
+end
 
